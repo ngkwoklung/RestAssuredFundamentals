89ab389dd268c96511745c0a54aa6b4cae8e4a36
Jimmy Ng <ngkwoklung@gmail.com>
2022-11-09 14:10:37 +0000

    Request specification

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class MyFirstTest extends TestConfig{
+
+    @Test
+    public void myFirstTest() {
+        given()
+                .log().all().
+        when().get("videogames").
+        then().
+                log().all();
+    }
+}
---
+++ src/test/java/TestConfig.java
@@ -0,0 +1,24 @@
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.BeforeClass;
+
+public class TestConfig {
+
+    @BeforeClass
+    public static void setup() {
+
+        //RestAssured.proxy("localhost",8888);
+
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.basePath = "/app/";
+        RestAssured.port = 8080;
+
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Accept", "application/json")
+                .build();
+
+        RestAssured.requestSpecification = requestSpecification;
+    }
+}
